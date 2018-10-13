f8fadd8a02faddd5c6d28ebfab4f9710b471d793
root <marcos_mol@hotmail.com>
2018-10-13 10:50:00 -0300

    Adicionado mais um time de MG (America-Mg)

--- teste.py
+++ teste.py
@@ -3,14 +3,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.model_selection import cross_val_predict
-dataset = pd.reader_csv('data.csv')
-dataset.count()
-tweets = dataset ['text'].values
-classes = dataset['Classificacao'].values
+#dataset = pd.reader_csv('data.csv')
+#dataset.count()
+#tweets = dataset ['text'].values
+#classes = dataset['Classificacao'].values
 vectorizer = CountVectorizer(analyzer='word')
-freq_tweets = vectorizer.fit_transform(tweets)
+#freq_tweets = vectorizer.fit_transform(tweets)
 modelo = MultinomialNB()
-modelo.fit(freq_tweets, classes)
+#modelo.fit(freq_tweets, classes)
 testes = ['Esse governo está no início, vamos ver o que vai dar',
           'Estou muito feliz com o governo de Minas esse ano',
           'O estado de Minas Gerais decretou calamidade financeira!!!',
@@ -18,7 +18,7 @@ testes = ['Esse governo está no início, vamos ver o que vai dar',
           'O governador de Minas é do PT']
 freq_testes = vectorizer.transform(testes)
 modelo.predict(freq_testes)
-resultado = cross_val_predict(modelo, freq_tweets, classes, cv=10)
+#resultado = cross_val_predict(modelo, freq_tweets, classes, cv=10)
 
 import csv
 import os
